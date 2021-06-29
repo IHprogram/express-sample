57bc02d7521f35fd4cf79aa1e893e5c7b4269053
IHprogram <ihm20202020@gmail.com>
2021-06-29 17:03:11 +0900

    フォームに入力した値を画面に表示させる処理を追加

--- server.js
+++ server.js
@@ -4,29 +4,31 @@ const ejs = require("ejs");
 
 const app = express();
 app.engine('ejs', ejs.renderFile); // EJSの読み込みを行っている。
-// app.use()は、アプリケーションにミドルウェアをバインドするためのもの。
-app.use(express.static('public'));
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }))
+// app.use()は、アプリケーションにミドルウェアをバインドするためのもの。
+// express.static関数は、静的なファイルを提供するためのミドルウェア。今回は省略。
+// app.use(express.static('public'));
+
+// const bodyParser = require('body-parser');
+// app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(express.urlencoded({ extended: false }))
 
 // トップページ
 app.get('/', (req, res) => {
-  const msg = 'これはメッセージです';
+  const msg = 'これはメッセージです(GET時)';
   res.render('index.ejs',
     {
-      title: 'Index',
       content: msg,
     })
 })
 
 // POST送信の処理
 app.post('/', (req, res) => {
-  const msg = 'これはPOST送信時のメッセージだ' + `あなたは${req.body.message}`
+  const msg = 'あなたが入力した値(POST時) →' + `${req.body.message}`
   res.render('index.ejs',
     {
-      title: 'Postedです！',
-      const: msg
+      content: msg
     }
   )
 })
